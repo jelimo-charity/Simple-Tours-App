90ab7ee1a5f68d8aba95ee1e76899103d01ad89a
jelimo-charity <charityjelimo893@gmail.com>
2024-01-21 08:58:00 +0300

    added styling

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -17,32 +17,43 @@ const Tour = () => {
   const removeTour = (id) => {
     const updatedTours = tours.filter((tour) => tour.id !== id);
     setTours(updatedTours);
+
+    // Check if readMore is true for the tour being removed, and reset it to false
+    if (readMore && id === readMore) {
+      setReadMore(false);
+    }
   };
 
   return (
-    <div className='toursContainer'>
-      <h1>Our Tours</h1>
-      {tours.map((tour) => (
-        <div key={tour.id} className='tourBox'>
-          <div className='imageBox'>
+    <section>
+      <div className="title">
+        <h1>Our tours</h1>
+        <div className="underline"></div>
+      </div>
+
+      <div className="contents">
+        {tours.map((tour) => (
+          <article key={tour.id} className='tourBox'>
             <img src={tour.image} width={400} alt={tour.name} />
-          </div>
-          <div className='details'>
-            <p>{tour.name}</p>
-            <p>${tour.price}</p>
+            <div className='details'>
+              <p className='name'>{tour.name}</p>
+              <p className='price'>${tour.price}</p>
+            </div>
             <div className="infos">
               <p>
-                {readMore ? tour.info : `${tour.info.substring(0, 200)}`}
-                <button onClick={() => setReadMore(!readMore)}>
-                  {readMore ? 'show less' : 'read more'}
+                {readMore && tour.id === readMore
+                  ? tour.info
+                  : `${tour.info.substring(0, 200)}`}
+                <button onClick={() => setReadMore(readMore === tour.id ? false : tour.id)}>
+                  {readMore === tour.id ? 'show less' : 'read more'}
                 </button>
               </p>
-              <button onClick={() => removeTour(tour.id)}>Not Interested</button>
+              <button className='deletebtn' onClick={() => removeTour(tour.id)}>Not Interested</button>
             </div>
-          </div>
-        </div>
-      ))}
-    </div>
+          </article>
+        ))}
+      </div>
+    </section>
   );
 };
 
